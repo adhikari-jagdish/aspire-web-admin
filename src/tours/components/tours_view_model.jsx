29826e9e44c9d6761c88633acf8b40db4d120e1f
Jagadish <adhikari.jagdish5200@gmail.com>
2025-07-09 09:11:02 +0545

    Design changes in Tour View Model

--- src/tours/components/tours_view_model.jsx
+++ src/tours/components/tours_view_model.jsx
@@ -8,8 +8,7 @@ import itinerary from "../../assets/icons/itinerary.svg";
 import location from "../../assets/icons/location.svg";
 import packageRate from "../../assets/icons/packageRate.svg";
 import { useRef } from "react";
-import { FaRupeeSign, FaDollarSign, FaEuroSign } from 'react-icons/fa';
-
+import { FaRupeeSign, FaDollarSign, FaEuroSign } from "react-icons/fa";
 
 const titles = [
   { name: "Trip Highlights", label: "tripHighlights" },
@@ -80,7 +79,7 @@ const ToursViewModel = ({
       });
     }
   };
-const tableInsideBorder = "text-center border"
+  const tableInsideBorder = "text-center border";
   return (
     <Modal
       opened={openedView}
@@ -92,10 +91,10 @@ const tableInsideBorder = "text-center border"
         title: {
           fontSize: "34px",
           color: "#0890cf",
-          fontWeight: 700
+          fontWeight: 700,
         },
         content: {
-          scrollbarWidth: "none", 
+          scrollbarWidth: "none",
         },
       }}
     >
@@ -108,18 +107,19 @@ const tableInsideBorder = "text-center border"
             {/* <Title style={{ fontWeight: "500", fontSize: "20px" }} order={4}>
                 Image
               </Title> */}
-            <div className="w-full h-[80vh] pt-[56.25%] relative">
+            <div className="w-full relative h-[40vh]">
               <Image
                 src={tour.image}
                 alt="Travel Theme"
+                fit="fill"
+                withPlaceholder
+                fallbackSrc="/assets/images/fallback-image.jpg"
                 radius="md"
-                h="100%"
-                w="100%"
-                fit="cover"
-                withPlaceHolder
-                fallbackSrc="https://placehold.co/600x400?text=Placeholder"
-                className="absolute top-0 "
+                className="absolute top-0 left-0 w-full h-full"
               />
+              <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black/40 text-white text-3xl font-semibold">
+                PLACEHOLDER IMAGE
+              </div>
             </div>
           </>
         )}
@@ -159,50 +159,55 @@ const tableInsideBorder = "text-center border"
 
               if (key === "tripHighlights" && Array.isArray(value)) {
                 content = (
-                  <ul className="space-y-3">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-3">
                     {value.map((trip, idx) => (
-                      <div key={idx} className="flex items-center gap-2">
+                      <div
+                        key={idx}
+                        className="flex items-start gap-3 border border-dotted border-blue-300 rounded-md p-3"
+                      >
                         <img
                           src={tripHighlights[idx]?.icon}
                           onError={(e) => {
-                            e.target.onerror = null; // Prevent infinite loops if fallback img also fails to laod
+                            e.target.onerror = null;
                             e.target.src =
                               "https://img.favpng.com/5/1/8/no-icon-png-favpng-PdDFZn3LtBiKi6JEapF5jAtgi.jpg";
                           }}
                           alt="Icon"
-                          className="w-8 mt-2"
+                          className="w-10 h-10 object-contain"
                         />
-                        <div className="pt-2">
-                          <li>
-                            <span className="font-[600]">Title</span>:{" "}
-                            {tripHighlights[idx]?.title}{" "}
-                          </li>
-                          <li>
-                            <span className="font-[600]">Description</span>:{" "}
-                            {trip.description}
-                          </li>
+                        <div className="space-y-1 text-sm">
+                          <div className="font-semibold">
+                            {tripHighlights[idx]?.title}
+                          </div>
+                          <div>{trip.description}</div>
                         </div>
                       </div>
                     ))}
-                  </ul>
+                  </div>
                 );
               } else if (key === "destinationIds") {
                 content = (
-                  <div className="flex gap-3 flex-wrap w-[500px] pl-2">
-                    {destinations?.map((d) => (
-                      <li className="list-none ring w-fit rounded-full   px-2 py-1 mt-2">
+                  <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 max-w-[500px] pl-2 mt-3">
+                    {destinations?.map((d, idx) => (
+                      <div
+                        key={idx}
+                        className="border border-gray-500 rounded-[5px] px-3 py-1 text-sm text-center"
+                      >
                         {d.title || "N/A"}
-                      </li>
+                      </div>
                     ))}
                   </div>
                 );
               } else if (key === "travelThemeIds") {
                 content = (
-                  <div className="flex gap-3 flex-wrap w-[500px] pl-2">
+                  <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 max-w-[600px] pl-2 mt-3">
                     {travelThemes?.map((t) => (
-                      <li className="list-none ring w-fit rounded-full  px-2 py-1 mt-2">
+                      <div
+                        key={t}
+                        className="border border-gray-500 rounded-[5px] px-2 py-1 text-sm text-center"
+                      >
                         {t.title || "N/A"}
-                      </li>
+                      </div>
                     ))}
                   </div>
                 );
@@ -214,7 +219,7 @@ const tableInsideBorder = "text-center border"
                 content = (
                   <div
                     ref={sectionsRef[t.label?.toLowerCase?.()]}
-                    style={{ position: "relative", paddingLeft: "5px" }}
+                    className="relative mt-[5px] text-justify"
                   >
                     <SafeHtml html={value} />
                     {discountBadge}
@@ -224,10 +229,10 @@ const tableInsideBorder = "text-center border"
                 content = (
                   <ul
                     ref={sectionsRef[t.label?.toLowerCase?.()]}
-                    className="space-y-3 "
+                    className="space-y-3 mt-3"
                   >
                     {value.map((day, idx) => (
-                      <li className="bg-gray-100 p-2 rounded">
+                      <li className="bg-blue-50 p-2 rounded border border-dotted border-gray-400">
                         <strong>{day.dayAndTitle}</strong> <br />
                         <span>Details: </span> {day.details?.[0]}
                       </li>
@@ -248,42 +253,53 @@ const tableInsideBorder = "text-center border"
               } else if (key === "packageRate" && Array.isArray(value)) {
                 content = (
                   <div
-  ref={sectionsRef[t.label?.toLowerCase?.()]}
-  className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-5"
->
-  {value.map((p, idx) => (
-    <div
-      key={p._id || idx}
-      className="bg-white shadow-md rounded-lg overflow-hidden border"
-    >
-      <div className="bg-gray-100 p-3 border-b text-center text-lg font-bold ">
-        Hotel Category: {p.hotelCategory}
-      </div>
+                    ref={sectionsRef[t.label?.toLowerCase?.()]}
+                    className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-5"
+                  >
+                    {value.map((p, idx) => (
+                      <div
+                        key={p._id || idx}
+                        className="bg-white shadow-md rounded-lg overflow-hidden border"
+                      >
+                        <div className="bg-gray-100 p-3 border-b text-center text-lg font-bold ">
+                          Hotel Category: {p.hotelCategory}
+                        </div>
 
-      <div className="p-4">
-        <table className="w-full text-sm text-left text-gray-700">
-          <thead className="bg-gray-50 border-b">
-            <tr>
-              <th className="px-4 py-2 border-r">NPR</th>
-              <th className="px-4 py-2 border-r">INR</th>
-              <th className="px-4 py-2 border-r">USD</th>
-              <th className="px-4 py-2">EUR</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="bg-white hover:bg-gray-50 transition">
-              <td className="px-4 py-2 border-r"><strong className="text-xl">Rs.</strong> {p.rateInNPR}</td>
-              <td className="px-4 py-2 border-r"><strong className="text-xl">₹ </strong>{p.rateInINR}</td>
-              <td className="px-4 py-2 border-r"><strong className="text-xl">$</strong> {p.rateInUSD}</td>
-              <td className="px-4 py-2"><strong className="text-xl">€</strong> {p.rateInEUR}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-  ))}
-</div>
-
+                        <div className="p-4">
+                          <table className="w-full text-sm text-left text-gray-700">
+                            <thead className="bg-gray-50 border-b">
+                              <tr>
+                                <th className="px-4 py-2 border-r">NPR</th>
+                                <th className="px-4 py-2 border-r">INR</th>
+                                <th className="px-4 py-2 border-r">USD</th>
+                                <th className="px-4 py-2">EUR</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr className="bg-white hover:bg-gray-50 transition">
+                                <td className="px-4 py-2 border-r">
+                                  <strong className="text-xl">Rs.</strong>{" "}
+                                  {p.rateInNPR}
+                                </td>
+                                <td className="px-4 py-2 border-r">
+                                  <strong className="text-xl">₹ </strong>
+                                  {p.rateInINR}
+                                </td>
+                                <td className="px-4 py-2 border-r">
+                                  <strong className="text-xl">$</strong>{" "}
+                                  {p.rateInUSD}
+                                </td>
+                                <td className="px-4 py-2">
+                                  <strong className="text-xl">€</strong>{" "}
+                                  {p.rateInEUR}
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 );
               } else if (key === "duration") {
                 content = value + " day(s)";
@@ -307,16 +323,18 @@ const tableInsideBorder = "text-center border"
                   <Text tyle={{ fontSize: "18px" }}>{content || "N/A"}</Text>
 
                   {key === "travelThemeIds" && (
-                    <div className="mt-6 gap-3 flex flex-wrap justify-between w-full ">
+                    <div className="mt-15 gap-3 flex flex-wrap justify-between w-full border-1 border-solid border-blue-200 my-5 p-1">
                       {buttons.map((b) => (
                         <button
+                          key={b.name}
                           onClick={() => scrollToSection(b.name.toLowerCase())}
-                          className="border bg-blue-600 text-white flex items-center justify-center  gap-2 rounded p-1 cursor-pointer hover:bg-blue-700 outline-0"
+                          className=" text-black font-black flex items-center justify-center gap-2 rounded px-4 py-2 cursor-pointer hover:bg-blue-300 outline-0"
                         >
                           <img
                             src={b?.icon}
                             alt={`${b.name} Icon`}
-                            className="w-4"
+                            className="w-4 filter invert-0"
+                            style={{ filter: "none" }} // ensure icon stays black if it's SVG
                           />
                           {b.name}
                         </button>
@@ -328,7 +346,7 @@ const tableInsideBorder = "text-center border"
             })}
           </Stack>
         ) : (
-          <Text color="dimmed">No tour data available.</Text>
+          <Text c="dimmed">No tour data available.</Text>
         )}
       </div>
     </Modal>
